881dd70c3bc7fc704da25353aa30e42ecc83618f
dilmurad0ff <dilmurodovizzatbek108@gmail.com>
2021-11-25 15:01:35 +0500

    izzy card added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Abdurashid from './view/abdurashid';
 import IslamCard from './view/islamkan12/IslamCard'
+import IzzyCard from './view/izzy/Izzy'
 import './App.css';
 
 function App() {
   return (
     <>
-      <Abdurashid />
-      <IslamCard/>
+    <IzzyCard />
+      {/* <Abdurashid />
+      <IslamCard/> */}
     </>
   );
 }
