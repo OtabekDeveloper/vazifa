90645c5ae2f816d65ae71c0df2ab4565ec8e4768
dilmurad0ff <dilmurodovizzatbek108@gmail.com>
2021-11-25 15:28:00 +0500

    izzy card added

---
+++ src/view/izzy/Izzy.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Izzy.css";
+import izzy from "../izzy/1.jpg";
+function Izzy() {
+  return (
+    <div>
+      <div className="izzyCard">
+        <img className="izzyImg" src={izzy} alt="izzy" />
+        <h1 className="izzyName">
+          Dilmurodov Izzatbek Muzaffar o'gli 2002-yil 21-iyunda tug'ilgan.
+          Xozirda Web Dasturlash bo'yicha Technobeck Programming Centerda middle
+          darajasida ishlayman
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Izzy;
