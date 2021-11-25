0685c18e6ce86157e42293b0d89294c8c2dab352
ElbekMadaminov <elbekmadaminov997@gmail.com>
2021-11-25 18:14:32 +0500

    Elbek ham qo'shdi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Abdurashid from './view/abdurashid';
 import IslamCard from './view/islamkan12/IslamCard'
+import Elbekjon from './view/Elbek/Elbek'
 import './App.css';
 import Abdulboriy from './view/Abdulboriy/Abdulboriy'
 import Izzy from './view/izzy/Izzy';
@@ -11,6 +12,7 @@ function App() {
       <IslamCard/>
       <Abdulboriy/>
       <Izzy />
+      <Elbekjon />
 
     </>
   );
---
+++ src/view/Elbek/Elbek.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Elbek.css'
+import Elbekjon from "../Elbek/rasm1.jpg";
+function Elbek() {
+  return (
+    <>
+      <div className='container'>
+         
+           <img className="imgElbek" src={Elbekjon} alt="" />
+           <div className="card ">
+           <h1>
+           '' Maktabga bor, yaxshi bilim ol va xavsiz ish o'rniga ega bo'l'', 
+           degan maslaxat, agar 1930-yilgacha tug' ilgan bo'lsangiz, siz uchun o'rinli edi. 
+           Lekin 1930-yildan keyin tug'ilganlar uchun bu yomon maslahat.)
+<br/>
+Robert Kiyosaki
+           </h1>
+           </div>
+      </div>
+    </>
+  );
+}
+
+export default Elbek;
