676a708d5be2635d615001a6aac97765c65058bc
Abdulaxad <wonderboy1398@gmail.com>
2021-11-26 20:34:58 +0500

    Abdulaxad xam qo'shdi

---
+++ src/view/Abdulaxad/Abdulaxad.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "./Abdulahad.jpg"
+
+function Abdulaxad(){
+    return(
+        <div className="container">
+        <div className="AlisheroffCard">
+            <div className="personalImage">
+                <img className="myImage" src={Image} />
+            </div>
+            <div className="myCardInfo">
+                <h2>My Personal Info</h2>
+                <h3>Arapov Abdulaxad</h3>
+                <h3>Frontend Developer</h3>
+                <h3>Tg: @Alisheroff13</h3>
+            </div>
+        </div>
+    </div>
+    )
+} 
+export default Abdulaxad;
